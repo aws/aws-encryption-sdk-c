29685ede3aadfe276577f0e4673bad9e1b8d4925
David Koenig <dkkoenig@amazon.com>
2018-05-11 09:13:07 -0700

    A few changes
    - Use void pointers in functions to allow for new struct types later
    - Fix bug: rotate through master keys as you produce encrypted data keys
    - single error clause to reduce code repetition

--- include/aws/cryptosdk/materials.h
+++ include/aws/cryptosdk/materials.h
@@ -72,8 +72,17 @@ struct aws_cryptosdk_master_key {
                             uint16_t alg_id);
 };
 
+struct aws_cryptosdk_private_key {
+    //FIXME: implement
+};
+
+struct aws_cryptosdk_public_key {
+    //FIXME: implement
+};
+
 struct aws_cryptosdk_key_pair {
-    // FIXME: implement
+    struct aws_cryptosdk_private_key private_key;
+    struct aws_cryptosdk_public_key public_key;
 };
 
 struct aws_cryptosdk_encryption_materials {
@@ -81,14 +90,14 @@ struct aws_cryptosdk_encryption_materials {
     struct aws_cryptosdk_unencrypted_data_key unencrypted_data_key;
     struct aws_array_list encrypted_data_keys; // list of struct aws_cryptosdk_encrypted_data_key objects
     struct aws_common_hash_table * enc_context;
-    struct aws_cryptosdk_key_pair trailing_signature_key;
+    struct aws_cryptosdk_key_pair trailing_signature_key_pair;
     uint16_t alg_id;
 };
 
 struct aws_cryptosdk_decryption_materials {
     struct aws_allocator * alloc;
     struct aws_cryptosdk_unencrypted_data_key unencrypted_data_key;
-    struct aws_cryptosdk_key_pair trailing_signature_key;
+    struct aws_cryptosdk_public_key trailing_signature_key;
 };
 
 struct aws_cryptosdk_materials_manager {
@@ -106,30 +115,36 @@ struct aws_cryptosdk_materials_manager {
      * and puts address at encryption_materials. If plaintext_size is nonzero, it may be used
      * in determining the encryption materials. If it is zero, it will be ignored.
      */
-    int (*generate_encryption_materials)(struct aws_cryptosdk_materials_manager * self,
-                                         struct aws_cryptosdk_encryption_materials ** encryption_materials,
-                                         struct aws_common_hash_table * enc_context,
-                                         size_t plaintext_size);
+    int (*generate_encryption_materials)(void * manager,
+                                         void ** encryption_materials,
+                                         void * args,
+                                         struct aws_common_hash_table * enc_context);
 
     /**
      * Checks whether any of the provided list of encrypted data keys can be decrypted by the
      * materials manager. On success, allocates decryption materials object and puts address at
      * decryption_materials.
      */
-    int (*generate_decryption_materials)(struct aws_cryptosdk_materials_manager * self,
-                                         struct aws_cryptosdk_decryption_materials ** decryption_materials,
+    int (*generate_decryption_materials)(void * manager,
+                                         void ** decryption_materials,
+                                         void * args,
                                          const struct aws_array_list * encrypted_data_keys,
                                          struct aws_common_hash_table * enc_context);
 
 };
 
-int aws_cryptosdk_default_generate_encryption_materials(struct aws_cryptosdk_materials_manager * self,
-                                                        struct aws_cryptosdk_encryption_materials ** encryption_materials,
+int aws_cryptosdk_default_generate_encryption_materials(void * manager,
+                                                        void ** encryption_materials,
+                                                        void * args,
                                                         struct aws_common_hash_table * enc_context,
                                                         size_t plaintext_size);
 
 struct aws_cryptosdk_encryption_materials * aws_cryptosdk_alloc_encryption_materials(struct aws_allocator * alloc, size_t num_keys);
 
 void aws_cryptosdk_free_encryption_materials(struct aws_cryptosdk_encryption_materials * enc_mat);
+
+// TODO: implement and move somewhere else possibly
+// should this allocate a key pair struct or write to an existing one?
+int generate_trailing_signature_key_pair(struct aws_cryptosdk_key_pair * key_pair, uint16_t alg_id);
 
 #endif // AWS_CRYPTOSDK_MATERIALS_H
--- source/materials.c
+++ source/materials.c
@@ -1,61 +1,67 @@
 #include <aws/cryptosdk/materials.h>
 
-int aws_cryptosdk_default_generate_encryption_materials(struct aws_cryptosdk_materials_manager * self,
-                                                        struct aws_cryptosdk_encryption_materials ** encryption_materials,
+int aws_cryptosdk_default_generate_encryption_materials(void * manager,
+                                                        void ** encryption_materials,
+                                                        void * args,
                                                         struct aws_common_hash_table * enc_context,
                                                         size_t plaintext_size) {
     int ret;
+    struct aws_cryptosdk_materials_manager * self = (struct aws_cryptosdk_materials_manager *) manager;
+    struct aws_cryptosdk_encryption_materials ** output = (struct aws_cryptosdk_encryption_materials **) encryption_materials;
 
     size_t num_keys = self->master_keys.current_size;
     size_t key_idx;
 
-    struct aws_cryptosdk_master_key * master_key;
-
-    ret = aws_array_list_get_at_ptr(&self->master_keys, (void **)&master_key, 0);
-    if (ret) {
-        return aws_raise_error(ret);
-    }
-
     struct aws_cryptosdk_encryption_materials * enc_mat;
+
     enc_mat = aws_cryptosdk_alloc_encryption_materials(self->alloc, num_keys);
-    if (!enc_mat) {
-        return aws_raise_error(AWS_ERROR_OOM);
-    }
+    if (!enc_mat) { ret = AWS_ERROR_OOM; goto ERROR; }
+
     enc_mat->enc_context = enc_context;
     enc_mat->alg_id = self->alg_id;
 
     /* Produce unencrypted data key and first encrypted data key from the first master key. */
+    struct aws_cryptosdk_master_key * master_key;
+    ret = aws_array_list_get_at_ptr(&self->master_keys, (void **)&master_key, 0);
+    if (ret) goto ERROR;
+
     struct aws_cryptosdk_encrypted_data_key * encrypted_data_key;
     ret = aws_array_list_get_at_ptr(&enc_mat->encrypted_data_keys, (void **)&encrypted_data_key, 0);
-    if (ret) {
-        aws_cryptosdk_free_encryption_materials(enc_mat);
-        return aws_raise_error(ret);
-    }
+    if (ret) goto ERROR;
 
     ret = master_key->generate_data_key(master_key, &enc_mat->unencrypted_data_key, encrypted_data_key, enc_context, self->alg_id);
-    if (ret) {
-        aws_cryptosdk_free_encryption_materials(enc_mat);
-        return aws_raise_error(ret);
-    }
+    if (ret) goto ERROR;
 
     /* Re-encrypt unencrypted data key with each other master key. */
     for (key_idx = 1 ; key_idx < num_keys ; ++key_idx) {
+        ret = aws_array_list_get_at_ptr(&self->master_keys, (void **)&master_key, key_idx);
+        if (ret) goto ERROR;
+
         ret = aws_array_list_get_at_ptr(&enc_mat->encrypted_data_keys, (void **)&encrypted_data_key, key_idx);
-        if (ret) {
-            aws_cryptosdk_free_encryption_materials(enc_mat);
-            return aws_raise_error(ret);
-        }
+        if (ret) goto ERROR;
 
         ret = master_key->encrypt_data_key(master_key, &enc_mat->unencrypted_data_key, encrypted_data_key, enc_context, self->alg_id);
-        if (ret) {
-            aws_cryptosdk_free_encryption_materials(enc_mat);
-            return aws_raise_error(ret);
-        }
+        if (ret) goto ERROR;
     }
-    // FIXME: how do we get enc_mat->trailing_signature_key?
+
+    struct aws_common_hash_element * p_elem;
+    int was_created;
+    generate_trailing_signature_key_pair(&enc_mat->trailing_signature_key_pair, self->alg_id);
+    ret = aws_common_hash_table_create(enc_context, (void *)"aws-crypto-public-key", &p_elem, &was_created);
+    if (was_created) {
+        // do we need to copy this? does it need to be encoded a particular way?
+        p_elem->value = (void *)&enc_mat->trailing_signature_key_pair.public_key;
+    } else {
+        // this would mean we had previously generated a public key for this encryption context, what happens here?
+        p_elem->value = (void *)&enc_mat->trailing_signature_key_pair.public_key;
+    }
 
     *encryption_materials = enc_mat;
     return AWS_OP_SUCCESS;
+
+ERROR:
+    aws_cryptosdk_free_encryption_materials(enc_mat);
+    return aws_raise_error(ret);
 }
 
 struct aws_cryptosdk_encryption_materials * aws_cryptosdk_alloc_encryption_materials(struct aws_allocator * alloc, size_t num_keys) {
