f7b8814119568cfcb72bae079b0c3b7326ee2dd2
Bryan Donlan <bdonlan@amazon.com>
2018-06-28 15:17:54 -0700

    Make the zero MK accept the values used for some of our test vectors

--- tests/lib/zero_mk.c
+++ tests/lib/zero_mk.c
@@ -26,11 +26,25 @@ void aws_cryptosdk_literally_null_edk(struct aws_cryptosdk_edk * edk) {
     edk->enc_data_key = aws_byte_buf_from_c_str(literally_null);
 }
 
+static bool buf_equals_string(const struct aws_byte_buf *buf, const char *cstr) {
+    size_t len = strlen(cstr);
+    return len == buf->len && !memcmp(buf->buffer, cstr, len);
+}
+
 static inline bool is_literally_null_edk(const struct aws_cryptosdk_edk * edk) {
-    size_t len = strlen(literally_null);
-    return edk->provider_id.len == len && !memcmp(edk->provider_id.buffer, literally_null, len) &&
-        edk->provider_info.len == len && !memcmp(edk->provider_info.buffer, literally_null, len) &&
-        edk->enc_data_key.len == len && !memcmp(edk->enc_data_key.buffer, literally_null, len);
+    if (buf_equals_string(&edk->provider_id, literally_null) &&
+        buf_equals_string(&edk->provider_info, literally_null) &&
+        buf_equals_string(&edk->enc_data_key, literally_null)
+    ) return true;
+
+    // Some older test vectors use "zero-key" / "provider info" / "\0" as their test data
+
+    if (buf_equals_string(&edk->provider_id, "zero-key") &&
+        buf_equals_string(&edk->provider_info, "provider info") &&
+        edk->enc_data_key.len == 1 && edk->enc_data_key.buffer[0] == 0
+    ) return true;
+
+    return false;
 }
 
 static int zero_mk_generate_data_key(struct aws_cryptosdk_mk * mk,
