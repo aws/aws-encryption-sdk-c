202856135083c167ad808eaf2b7e5c6ae84be80f
David Koenig <dkkoenig@amazon.com>
2018-08-01 13:33:43 -0700

    remove abort in EDK list clean up

--- source/materials.c
+++ source/materials.c
@@ -48,8 +48,9 @@ void aws_cryptosdk_edk_list_clean_up(struct aws_array_list * edk_list) {
     size_t num_keys = edk_list->length;
     for (size_t key_idx = 0 ; key_idx < num_keys ; ++key_idx) {
         struct aws_cryptosdk_edk * edk;
-        if (aws_array_list_get_at_ptr(edk_list, (void **)&edk, key_idx)) abort();
-        aws_cryptosdk_edk_clean_up(edk);
+        if (!aws_array_list_get_at_ptr(edk_list, (void **)&edk, key_idx)) {
+            aws_cryptosdk_edk_clean_up(edk);
+        }
     }
     aws_array_list_clean_up(edk_list);
 }
