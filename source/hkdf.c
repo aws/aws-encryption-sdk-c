9fd3b4df72ac2beb3beb8580225cb643e47b1624
Tejeswini Sundaram <ttjsu@amazon.com>
2018-08-15 14:36:26 -0700

    minor changes to PR

--- source/hkdf.c
+++ source/hkdf.c
@@ -36,12 +36,11 @@ static int aws_cryptosdk_hkdf_extract(
     enum aws_cryptosdk_sha_version which_sha,
     const struct aws_byte_buf *salt,
     const struct aws_byte_buf *ikm) {
-    const static uint8_t zeroes[EVP_MAX_MD_SIZE] = { 0 };
-    const uint8_t *mysalt = NULL;
-    size_t mysalt_len = 0;
     const EVP_MD *evp_md = aws_cryptosdk_which_sha(which_sha);
     if (!evp_md) return aws_raise_error(AWS_CRYPTOSDK_ERR_UNSUPPORTED_FORMAT);
-
+    static const uint8_t zeroes[EVP_MAX_MD_SIZE] = { 0 };
+    const uint8_t *mysalt = NULL;
+    size_t mysalt_len = 0;
     if (salt->len) {
         mysalt = (uint8_t *)salt->buffer;
         mysalt_len = salt->len;
@@ -49,7 +48,6 @@ static int aws_cryptosdk_hkdf_extract(
         mysalt = zeroes;
         mysalt_len = EVP_MD_size(evp_md);
     }
-
     if (!HMAC(evp_md, mysalt, mysalt_len, ikm->buffer, ikm->len, prk, (unsigned int *)prk_len)) {
         aws_secure_zero(prk, sizeof(prk));
         return aws_raise_error(AWS_CRYPTOSDK_ERR_CRYPTO_UNKNOWN);
@@ -63,17 +61,16 @@ static int aws_cryptosdk_hkdf_expand(
     const uint8_t *prk,
     size_t prk_len,
     const struct aws_byte_buf *info) {
+    const EVP_MD *evp_md = aws_cryptosdk_which_sha(which_sha);
+    if (!evp_md) return aws_raise_error(AWS_CRYPTOSDK_ERR_UNSUPPORTED_FORMAT);
     HMAC_CTX ctx;
     uint8_t t[EVP_MAX_MD_SIZE];
     size_t n = 0;
     size_t t_len = 0;
     size_t bytes_to_write;
     size_t bytes_remaining = okm->len;
-    const EVP_MD *evp_md = aws_cryptosdk_which_sha(which_sha);
-    if (!evp_md) return aws_raise_error(AWS_CRYPTOSDK_ERR_UNSUPPORTED_FORMAT);
     size_t hash_len = EVP_MD_size(evp_md);
     HMAC_CTX_init(&ctx);
-
     if (!prk || !okm->len || !prk_len) goto err;
     n = (okm->len + hash_len - 1) / hash_len;
     if (n > 255) goto err;
@@ -112,15 +109,13 @@ static int aws_cryptosdk_openssl_hkdf_version(
     const struct aws_byte_buf *salt,
     const struct aws_byte_buf *ikm,
     const struct aws_byte_buf *info) {
-    EVP_PKEY_CTX *pctx = EVP_PKEY_CTX_new_id(EVP_PKEY_HKDF, NULL);
     const EVP_MD *evp_md = aws_cryptosdk_which_sha(which_sha);
-    const static uint8_t zeroes[EVP_MAX_MD_SIZE] = { 0 };
+    if (!evp_md) return aws_raise_error(AWS_CRYPTOSDK_ERR_UNSUPPORTED_FORMAT);
+    EVP_PKEY_CTX *pctx = EVP_PKEY_CTX_new_id(EVP_PKEY_HKDF, NULL);
+    if (!pctx) return aws_raise_error(AWS_CRYPTOSDK_ERR_CRYPTO_UNKNOWN);
+    static const uint8_t zeroes[EVP_MAX_MD_SIZE] = { 0 };
     const uint8_t *mysalt = NULL;
     size_t mysalt_len = 0;
-
-    if (!evp_md) return aws_raise_error(AWS_CRYPTOSDK_ERR_UNSUPPORTED_FORMAT);
-    if (!pctx) return aws_raise_error(AWS_CRYPTOSDK_ERR_CRYPTO_UNKNOWN);
-
     if (salt->len) {
         mysalt = (uint8_t *)salt->buffer;
         mysalt_len = salt->len;
--- tests/unit/t_hkdf.c
+++ tests/unit/t_hkdf.c
@@ -197,7 +197,7 @@ struct hkdf_test_vectors tv[] = {
 
     { .which_sha   = NOSHA,
       .ikm         = tv_6_ikm,
-      .ikm_len     = 10},
+      .ikm_len     = 10 },
 };
 
 int test_hkdf() {
@@ -213,11 +213,11 @@ int test_hkdf() {
             TEST_ASSERT_ERROR(
                 AWS_CRYPTOSDK_ERR_UNSUPPORTED_FORMAT,
                 aws_cryptosdk_hkdf(&myokm, tv[i].which_sha, &mysalt, &myikm, &myinfo));
+            aws_byte_buf_clean_up(&myokm);
             break;
         }
         if (aws_cryptosdk_hkdf(&myokm, tv[i].which_sha, &mysalt, &myikm, &myinfo)) return AWS_OP_ERR;
         if (memcmp(tv[i].okm_desired, myokm.buffer, myokm.len) != 0) return AWS_OP_ERR;
-        aws_byte_buf_clean_up(&myokm);
     }
     return AWS_OP_SUCCESS;
 }
