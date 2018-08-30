94cef74de9bf35385a0a62c44680fd27ac025f97
Lauren Pick <laurpick@amazon.com>
2018-08-30 16:10:38 -0400

    Fix to cbmc-batch.sh in response to PR comment

--- .cbmc-batch/cbmc-batch.sh
+++ .cbmc-batch/cbmc-batch.sh
@@ -13,9 +13,7 @@
 # implied. See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -e
-set -u
-set -x
+set -eux
 
 if [ "$#" -eq 0 ]; then
     echo "Specify option -s to start jobs, -e to end jobs, and -c to cleanup bookkeeping"
