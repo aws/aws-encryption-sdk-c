7ae1ec8d5605c9d69ea454497cd7a596ad5d4ac5
David Koenig <dkkoenig@amazon.com>
2018-09-19 17:41:32 +0000

    Add warning comment to edk.h

--- include/aws/cryptosdk/edk.h
+++ include/aws/cryptosdk/edk.h
@@ -19,6 +19,13 @@
 #include <aws/common/array_list.h>
 #include <aws/common/byte_buf.h>
 
+/*
+ * This public interface to the encrypted data key (EDK) objects is provided for
+ * developers of CMMs and keyrings only. If you are a user of the AWS Encryption
+ * SDK for C and you are not developing your own CMMs and/or keyrings, you do not
+ * need to use it and you should not do so.
+ */
+
 struct aws_cryptosdk_edk {
     struct aws_byte_buf provider_id;
     struct aws_byte_buf provider_info;
