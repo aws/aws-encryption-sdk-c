218c5a185ba9ac832603d0362691ada5369ad54a
Tejeswini Sundaram <ttjsu@amazon.com>
2018-09-21 12:08:58 -0700

    Replaced the test vectors and rsa_key_pair to use a RSA key size 2048 instead of 4096, to improve the performace of the unit test suite

--- tests/lib/raw_rsa_keyring_test_vectors.h
+++ tests/lib/raw_rsa_keyring_test_vectors.h
@@ -18,11 +18,15 @@
 #include <aws/cryptosdk/cipher.h>
 #include <aws/cryptosdk/materials.h>
 
+enum rsa_function { 
+    AWS_CRYPTOSDK_RSA_ENCRYPT,
+    AWS_CRYPTOSDK_RSA_DECRYPT
+};
 struct aws_cryptosdk_keyring *raw_rsa_keyring_tv_new(
-    struct aws_allocator *alloc, enum aws_cryptosdk_rsa_padding_mode rsa_padding_mode);
+    struct aws_allocator *alloc, enum aws_cryptosdk_rsa_padding_mode rsa_padding_mode, enum rsa_function rsa_fn);
 
 struct aws_cryptosdk_keyring *raw_rsa_keyring_tv_new_with_wrong_key(
-    struct aws_allocator *alloc, enum aws_cryptosdk_rsa_padding_mode rsa_padding_mode);
+    struct aws_allocator *alloc, enum aws_cryptosdk_rsa_padding_mode rsa_padding_mode, enum rsa_function rsa_fn);
 
 struct raw_rsa_keyring_test_vector {
     enum aws_cryptosdk_rsa_padding_mode rsa_padding_mode;
