1d06824a7eaceea13e0c0647b927cb7fad3682a5
Bryan Donlan <bdonlan@amazon.com>
2018-11-02 14:53:04 -0700

    Clarify comment

--- include/aws/cryptosdk/edk.h
+++ include/aws/cryptosdk/edk.h
@@ -71,7 +71,7 @@ AWS_CRYPTOSDK_API
 int aws_cryptosdk_edk_init_clone(struct aws_allocator *alloc, struct aws_cryptosdk_edk *dest, const struct aws_cryptosdk_edk *src);
 
 /**
- * _Copies_ all EDKs in the list at src, appending the copies to dest.
+ * _Copies_ all EDKs in the list at src, appending the copies to dest. dest must already be initialized.
  *
  * On failure, the destination list is unchanged.
  */
