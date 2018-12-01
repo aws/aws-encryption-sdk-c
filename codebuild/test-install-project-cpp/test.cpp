2260651172b200414a65ce177cca2b24fc5166a8
Bryan Donlan <bdonlan@amazon.com>
2018-11-30 16:46:36 -0800

    Add CI tests for our install logic
    Right now these tests simply check that things build and can call across the
    library boundary. We should add some meaningful calls to these in the future.

---
+++ codebuild/test-install-project-cpp/test.cpp
@@ -0,0 +1,10 @@
+#include <aws/cryptosdk/session.h>
+#include <aws/cryptosdk/kms_keyring.h>
+
+int main() {
+//    aws_cryptosdk_session_destroy(NULL);
+
+    Aws::Cryptosdk::KmsKeyring::Builder();
+
+    return 0;
+}
