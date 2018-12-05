0c43f99c3e602a24406e85497f26bd71f843c318
David Koenig <dkkoenig@amazon.com>
2018-12-05 20:14:06 +0000

    Add doc comments to keyring_trace.h

--- include/aws/cryptosdk/keyring_trace.h
+++ include/aws/cryptosdk/keyring_trace.h
@@ -41,6 +41,9 @@
  *
  * The motivating example of a wrapping key is a KMS CMK, for which the name_space
  * is "aws-kms" and the name is the key ARN.
+ *
+ * Do not handle this structure by itself. It exists primarily as a semantic
+ * label to link the name_space and name within the trace record.
  */
 struct aws_cryptosdk_wrapping_key {
     struct aws_string *name_space;
@@ -69,28 +72,46 @@ struct aws_cryptosdk_keyring_trace_record {
 extern "C" {
 #endif
 
-// TODO: add doc comments
-
+/**
+ * Add a record to the trace with the specified name_space, name, and flags.
+ * Makes duplicates of name_space and name strings. Will be deallocated
+ * when the keyring trace object is cleared or cleaned up.
+ */
 AWS_CRYPTOSDK_API
 int aws_cryptosdk_keyring_trace_add_record(struct aws_allocator *alloc,
-                                         struct aws_array_list *trace,
-                                         const struct aws_string *name_space,
-                                         const struct aws_string *name,
-                                         uint32_t flags);
+                                           struct aws_array_list *trace,
+                                           const struct aws_string *name_space,
+                                           const struct aws_string *name,
+                                           uint32_t flags);
 
+/**
+ * Same as aws_cryptosdk_keyring_trace_add_record except it takes C strings
+ * instead of AWS strings.
+ */
 AWS_CRYPTOSDK_API
 int aws_cryptosdk_keyring_trace_add_record_c_str(struct aws_allocator *alloc,
-                                               struct aws_array_list *trace,
-                                               const char *name_space,
-                                               const char *name,
-                                               uint32_t flags);
+                                                 struct aws_array_list *trace,
+                                                 const char *name_space,
+                                                 const char *name,
+                                                 uint32_t flags);
 
+/**
+ * Initialize a keyring trace.
+ */
 AWS_CRYPTOSDK_API
-int aws_cryptosdk_keyring_trace_init(struct aws_allocator *alloc, struct aws_array_list *trace);
+int aws_cryptosdk_keyring_trace_init(struct aws_allocator *alloc,
+                                     struct aws_array_list *trace);
 
+/**
+ * Deallocate all memory from a keyring trace.
+ */
 AWS_CRYPTOSDK_API
 void aws_cryptosdk_keyring_trace_clean_up(struct aws_array_list *trace);
 
+/**
+ * Deallocate and remove all records from a keyring trace, but do not
+ * deallocate the keyring trace itself.
+ */
 AWS_CRYPTOSDK_API
 void aws_cryptosdk_keyring_trace_clear(struct aws_array_list *trace);
 
