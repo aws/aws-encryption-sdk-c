5e44c187a962328209ba7b58e0919a14ac0ebbf4
Rajeev Joshi <43651355+jorajeev@users.noreply.github.com>
2018-12-06 07:04:32 -0800

    Code cleanup

--- examples/file_encrypt_decrypt.cpp
+++ examples/file_encrypt_decrypt.cpp
@@ -27,22 +27,31 @@
 
 #include <aws/core/client/ClientConfiguration.h>
 
-const char *KEY_ARN = "arn:aws:kms:us-west-2:658956600833:key/b3537ef1-d8dc-4780-9f5a-55776cbb2f7f";
 const size_t INITIAL_CAPACITY = 16 * 1024;
 
-static void resize_buffer(aws_byte_buf * buffer, size_t desired_capacity) {
-    if (buffer->capacity < desired_capacity) {
-        aws_byte_buf old_buf = *buffer;
-        aws_byte_cursor cursor = aws_byte_cursor_from_buf(&old_buf);       
-        aws_byte_buf_init(buffer, old_buf.allocator, desired_capacity);
-        int status = aws_byte_buf_append(buffer, &cursor);
-        aws_byte_buf_clean_up_secure(&old_buf);
+struct byte_buf {
+    uint8_t * buffer;
+    size_t len; // number of bytes currently in use
+    size_t capacity; // allocated size
+};
+
+static void init_buffer(struct byte_buf * buf, size_t capacity, struct aws_allocator * allocator) {
+    buf->buffer = (uint8_t *)aws_mem_acquire(allocator, capacity);
+    if (!buf->buffer) abort();
+    buf->capacity = capacity;
+    buf->len = 0;
+}
+
+static void resize_buffer(struct byte_buf * buf, size_t desired_capacity, struct aws_allocator * allocator) {
+    if (buf->capacity < desired_capacity) {
+        int status = aws_mem_realloc(allocator, (void **)&buf->buffer, buf->capacity, desired_capacity);
         if (status != AWS_OP_SUCCESS) abort();
+        buf->capacity = desired_capacity;
     }
 }
 
-static void process_file(FILE * output_fp, FILE * input_fp, aws_cryptosdk_mode mode, aws_allocator * allocator) {
-    auto kms_keyring = Aws::Cryptosdk::KmsKeyring::Builder().Build({KEY_ARN});
+static void process_file(FILE * output_fp, FILE * input_fp, aws_cryptosdk_mode mode, char const * key_arn, struct aws_allocator * allocator) {
+    auto kms_keyring = Aws::Cryptosdk::KmsKeyring::Builder().Build({key_arn});
 
     struct aws_cryptosdk_cmm *cmm = aws_cryptosdk_default_cmm_new(allocator, kms_keyring);
     if (!cmm) abort();
@@ -52,15 +61,14 @@ static void process_file(FILE * output_fp, FILE * input_fp, aws_cryptosdk_mode m
     if (!session) abort();
     aws_cryptosdk_cmm_release(cmm);
 
-    aws_byte_buf input_buffer, output_buffer;
-    int aws_status = aws_byte_buf_init(&input_buffer, allocator, INITIAL_CAPACITY);
-    if (aws_status != AWS_OP_SUCCESS) abort();
-
-    aws_status = aws_byte_buf_init(&output_buffer, allocator, INITIAL_CAPACITY);
-    if (aws_status != AWS_OP_SUCCESS) abort();
+    struct byte_buf input_buffer, output_buffer;
+    init_buffer(&input_buffer, INITIAL_CAPACITY, allocator);
+    init_buffer(&output_buffer, INITIAL_CAPACITY, allocator);
 
     size_t total_input_consumed = 0;
     size_t total_output_produced = 0;
+
+    int aws_status = AWS_OP_SUCCESS;
 
     while (!aws_cryptosdk_session_is_done(session)) {
         if (!feof(input_fp) && (input_buffer.len < input_buffer.capacity)) {
@@ -83,7 +91,7 @@ static void process_file(FILE * output_fp, FILE * input_fp, aws_cryptosdk_mode m
         if (aws_status != AWS_OP_SUCCESS) break;
         total_input_consumed += input_done;
 
-        if (input_done < input_buffer.len) {
+        if ((input_done > 0) && (input_done < input_buffer.len)) {
             // If not all input was consumed, move what's left over to the beginning of the buffer
             memmove(input_buffer.buffer, &input_buffer.buffer[input_done], input_buffer.len - input_done);
         }
@@ -97,12 +105,16 @@ static void process_file(FILE * output_fp, FILE * input_fp, aws_cryptosdk_mode m
             total_output_produced += num_written;
         }
 
+        if (!input_done && !output_done) { // We made no progress; something is wrong
+            break;
+        }
+
         // Determine how much buffer space we need to make progress, and resize if necessary
         size_t input_needed, output_needed;
         aws_cryptosdk_session_estimate_buf(session, &output_needed, &input_needed);
 
-        resize_buffer(&output_buffer, output_needed);
-        resize_buffer(&input_buffer, input_needed);
+        resize_buffer(&output_buffer, output_needed, allocator);
+        resize_buffer(&input_buffer, input_needed, allocator);
     }
 
     if (!aws_cryptosdk_session_is_done(session)) {
@@ -112,17 +124,18 @@ static void process_file(FILE * output_fp, FILE * input_fp, aws_cryptosdk_mode m
                (int)total_input_consumed, (int)total_output_produced) ;
     }
 
-    aws_byte_buf_clean_up_secure(&input_buffer);
-    aws_byte_buf_clean_up_secure(&output_buffer);
+    aws_mem_release(allocator, input_buffer.buffer);
+    aws_mem_release(allocator, output_buffer.buffer);
+
     aws_cryptosdk_session_destroy(session);
 }
 
-void encrypt_file(FILE * ciphertext_fp, FILE * plaintext_fp, struct aws_allocator * allocator) {
-    process_file(ciphertext_fp, plaintext_fp, AWS_CRYPTOSDK_ENCRYPT, allocator);
+void encrypt_file(FILE * ciphertext_fp, FILE * plaintext_fp, char const * key_arn, struct aws_allocator * allocator) {
+    process_file(ciphertext_fp, plaintext_fp, AWS_CRYPTOSDK_ENCRYPT, key_arn, allocator);
 }
 
-void decrypt_file(FILE * plaintext_fp, FILE * ciphertext_fp, struct aws_allocator * allocator) {
-    process_file(plaintext_fp, ciphertext_fp, AWS_CRYPTOSDK_DECRYPT, allocator);
+void decrypt_file(FILE * plaintext_fp, FILE * ciphertext_fp, char const * key_arn, struct aws_allocator * allocator) {
+    process_file(plaintext_fp, ciphertext_fp, AWS_CRYPTOSDK_DECRYPT, key_arn, allocator);
 }
 
 int main(int argc, char * argv[]) {
@@ -133,13 +146,19 @@ int main(int argc, char * argv[]) {
 
     bool do_encrypt = !strcmp(argv[1], "-e");
 
-    FILE * input_fp = fopen(argv[2], "r");
+    char * key_arn = getenv("KEY_ARN");
+    if (key_arn == NULL) {
+        fprintf(stderr, "Environment variable KEY_ARN for the KMS key is not set");
+        exit(1);
+    }
+
+    FILE * input_fp = fopen(argv[2], "rb");
     if (!input_fp) {
         fprintf(stderr, "Could not open input file %s for reading; error %s\n", argv[2], strerror(errno));
         exit(1);
     }
 
-    FILE * output_fp = fopen(argv[3], "w");
+    FILE * output_fp = fopen(argv[3], "wb");
     if (!output_fp) {
         fprintf(stderr, "Could not open output file %s for writing; error %s\n", argv[3], strerror(errno));
         fclose(input_fp);
@@ -150,14 +169,13 @@ int main(int argc, char * argv[]) {
 
     Aws::SDKOptions::SDKOptions options;
     Aws::InitAPI(options);
-    enum { BUFFER_SIZE = 1024 };
 
     struct aws_allocator * allocator = aws_default_allocator();
 
     if (do_encrypt) {
-        encrypt_file(output_fp, input_fp, allocator);
+        encrypt_file(output_fp, input_fp, key_arn, allocator);
     } else {
-        decrypt_file(output_fp, input_fp, allocator);
+        decrypt_file(output_fp, input_fp, key_arn, allocator);
     }
 
     fclose(input_fp);
