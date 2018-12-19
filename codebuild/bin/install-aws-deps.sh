05fdbb60f5046fcfe0b1a4a7f978d4d427dc3ae0
David Koenig <dkkoenig@amazon.com>
2018-12-19 01:38:11 +0000

    fixed UNIX pinning version number of aws-sdk-cpp
    This code still is not correct because it does not use the correct
    version of aws-sdk-cpp for Windows builds, and because it needs to
    change to build the three aws-c* libraries BEFORE the aws-sdk-cpp

--- codebuild/bin/install-aws-deps.sh
+++ codebuild/bin/install-aws-deps.sh
@@ -64,6 +64,5 @@ for libtype in shared static; do
     build_pkg $root/install https://github.com/awslabs/aws-c-common.git v0.2.0 $CMAKE_ARGS
     build_pkg $root/install https://github.com/awslabs/aws-checksums.git v0.1.0 $CMAKE_ARGS
     build_pkg $root/install https://github.com/awslabs/aws-c-event-stream v0.1.0 $CMAKE_ARGS
-    build_pkg $root/install https://github.com/aws/aws-sdk-cpp.git 1.6.35 $CMAKE_ARGS -DBUILD_ONLY=kms -DENABLE_UNITY_BUILD=ON
+    build_pkg $root/install https://github.com/aws/aws-sdk-cpp.git 1.7.21 $CMAKE_ARGS -DBUILD_ONLY=kms -DENABLE_UNITY_BUILD=ON
 done
-
