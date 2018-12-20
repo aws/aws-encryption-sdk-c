fd2eee19b4721d7b42ec2727672b94f72d93704a
David Koenig <dkkoenig@amazon.com>
2018-12-20 18:47:04 +0000

    Fix a couple of small typos in caching example
    Example does build and run, but it hangs. Created issue #260
    to fix that problem.

--- examples/cache_encrypt_decrypt.cpp
+++ examples/cache_encrypt_decrypt.cpp
@@ -140,7 +140,7 @@ struct aws_cryptosdk_cmm *setup_cmm(struct aws_allocator *alloc, const char *key
 
 int main(int argc, char **argv) {
     if (argc < 2) {
-        std::cerr << "Usage: " << arv[0] << " key_arn string1 [string2 string2 ...]" << std::endl;
+        std::cerr << "Usage: " << argv[0] << " key_arn string1 [string2 string3 ...]" << std::endl;
         return 1;
     }
 
