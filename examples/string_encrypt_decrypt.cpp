74f47c16931279be00e4c6d4a9b576ba049ddaf0
David Koenig <dkkoenig@amazon.com>
2019-01-02 23:32:45 +0000

    Add encryption context to string example

--- examples/string_encrypt_decrypt.cpp
+++ examples/string_encrypt_decrypt.cpp
@@ -17,6 +17,15 @@
 #include <aws/cryptosdk/default_cmm.h>
 #include <aws/cryptosdk/session.h>
 
+/* Declares AWS strings of type (static const struct aws_string *),
+ * which are immutable string that do not need to be deallocated.
+ * aws_string_destroy is a no-op on AWS strings declared with this macro.
+ *
+ * This strings will be the key-value pair used in the encryption context.
+ */
+AWS_STATIC_STRING_FROM_LITERAL(enc_ctx_key, "Example");
+AWS_STATIC_STRING_FROM_LITERAL(enc_ctx_value, "Value");
+
 void encrypt_string(
     struct aws_allocator *alloc,
     const char *key_arn,
@@ -41,12 +50,45 @@ void encrypt_string(
 
     assert(AWS_OP_SUCCESS == aws_cryptosdk_session_set_message_size(session, plaintext_len));
 
+    /* The encryption context is an AWS hash table where both the key and value
+     * types are AWS strings. Both AWS hash tables and AWS strings are defined in
+     * the aws-c-common library.
+     *
+     * This function gives us a mutable pointer to the encryption context, allowing
+     * us to add or modify items. It only works for encrypt sessions and only
+     * before aws_cryptosdk_session_process is called. At other times, it returns NULL.
+     */
+    struct aws_hash_table *enc_ctx = aws_cryptosdk_session_get_enc_ctx_ptr_mut(session);
+
+    /* We add the key-value string pair defined at the top of this file to the
+     * encryption context.
+     *
+     * Note that if we were defining strings at runtime, we would need to use the
+     * aws_string_new... functions in aws-c-common instead of the
+     * AWS_STATIC_STRING_FROM_LITERAL macro. Regardless of which method we use of
+     * creating the strings, we must NOT destroy them. The encryption context hash
+     * table will own their memory, and non-static strings will be deallocated when
+     * the session is destroyed.
+     */
+    int was_created;
+    assert(
+        AWS_OP_SUCCESS == aws_hash_table_put(enc_ctx, (const void *)enc_ctx_key, (void *)enc_ctx_value, &was_created));
+    assert(was_created == 1);
+
     size_t plaintext_consumed;
-    assert(AWS_OP_SUCCESS == aws_cryptosdk_session_process(
-               session, ciphertext, ciphertext_buf_sz, ciphertext_len, plaintext, plaintext_len, &plaintext_consumed));
+    assert(
+        AWS_OP_SUCCESS ==
+        aws_cryptosdk_session_process(
+            session, ciphertext, ciphertext_buf_sz, ciphertext_len, plaintext, plaintext_len, &plaintext_consumed));
 
     assert(aws_cryptosdk_session_is_done(session));
     assert(plaintext_consumed == plaintext_len);
+
+    /* This call deallocates all of the memory allocated in this function, including
+     * the keyring and CMM, since we already released their pointers. It also
+     * deallocates the encryption context in the session, which in turn calls
+     * aws_string_destroy on every key and value string in the encryption context.
+     */
     aws_cryptosdk_session_destroy(session);
 }
 
@@ -73,11 +115,30 @@ void decrypt_string(
     aws_cryptosdk_cmm_release(cmm);
 
     size_t ciphertext_consumed;
-    assert(AWS_OP_SUCCESS == aws_cryptosdk_session_process(
-               session, plaintext, plaintext_buf_sz, plaintext_len, ciphertext, ciphertext_len, &ciphertext_consumed));
+    assert(
+        AWS_OP_SUCCESS ==
+        aws_cryptosdk_session_process(
+            session, plaintext, plaintext_buf_sz, plaintext_len, ciphertext, ciphertext_len, &ciphertext_consumed));
 
     assert(aws_cryptosdk_session_is_done(session));
     assert(ciphertext_consumed == ciphertext_len);
+
+    /* The encryption context is stored in plaintext in the ciphertext format, and the
+     * AWS Encryption SDK detects it and uses it for decryption, so there is no need to
+     * provide it at decrypt time. After decryption is done, you can get a read-only
+     * pointer to the encryption context using this function.
+     */
+    const struct aws_hash_table *enc_ctx = aws_cryptosdk_session_get_enc_ctx_ptr(session);
+    assert(enc_ctx);
+
+    /* We retrieve the value associated with our known key. */
+    struct aws_hash_element *enc_ctx_kv_pair;
+    assert(AWS_OP_SUCCESS == aws_hash_table_find(enc_ctx, (const void *)enc_ctx_key, &enc_ctx_kv_pair));
+    assert(enc_ctx_kv_pair);
+    const struct aws_string *enc_ctx_value_decrypt = (const struct aws_string *)enc_ctx_kv_pair->value;
+
+    /* We verify that the encryption context value is what we expect. */
+    assert(aws_string_eq((const void *)enc_ctx_value, (const void *)enc_ctx_value_decrypt));
     aws_cryptosdk_session_destroy(session);
 }
 
