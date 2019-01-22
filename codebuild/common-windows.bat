3e73c184e1c201d754ea95220f9d2e8e35380cea
David Koenig <dkkoenig@amazon.com>
2019-01-22 23:58:39 +0000

    Modify common-windows.bat to install C++ SDK

--- codebuild/common-windows.bat
+++ codebuild/common-windows.bat
@@ -14,11 +14,11 @@ REM limitations under the License.
 rmdir/s/q deps
 mkdir deps
 cd deps
-git clone https://github.com/awslabs/aws-c-common.git || goto error
-mkdir c-common-build
-cd c-common-build
-cmake %* -DCMAKE_INSTALL_PREFIX=c:/deps -DCMAKE_BUILD_TYPE="Release" -DCMAKE_TOOLCHAIN_FILE=C:/vcpkg/scripts/buildsystems/vcpkg.cmake ../aws-c-common || goto error
-msbuild.exe aws-c-common.vcxproj /p:Configuration=Release || goto error
+git clone -b 1.7.36 https://github.com/aws/aws-sdk-cpp.git || goto error
+mkdir build-aws-sdk-cpp
+cd build-aws-sdk-cpp
+cmake %* -DCMAKE_INSTALL_PREFIX=c:/deps -DCMAKE_BUILD_TYPE="Release" -DCMAKE_TOOLCHAIN_FILE=C:/vcpkg/scripts/buildsystems/vcpkg.cmake ../aws-sdk-cpp || goto error
+msbuild.exe ALL_BUILD.vcxproj /p:Configuration=Release || goto error
 msbuild.exe INSTALL.vcxproj /p:Configuration=Release || goto error
 
 cd ..\..
@@ -26,7 +26,7 @@ cd ..\..
 rmdir/s/q build
 mkdir build
 cd build
-cmake %* -DCMAKE_INSTALL_PREFIX=c:/deps -DCMAKE_TOOLCHAIN_FILE=C:/vcpkg/scripts/buildsystems/vcpkg.cmake -DCMAKE_BUILD_TYPE="Release" -DFORCE_KMS_KEYRING_BUILD=ON -DAWS_ENC_SDK_END_TO_END_TESTS=ON ../ || goto error
+cmake %* -DCMAKE_INSTALL_PREFIX=c:/deps -DCMAKE_TOOLCHAIN_FILE=C:/vcpkg/scripts/buildsystems/vcpkg.cmake -DCMAKE_BUILD_TYPE="Release" -DBUILD_AWS_ENC_SDK_CPP=ON -DAWS_ENC_SDK_END_TO_END_TESTS=ON ../ || goto error
 msbuild.exe ALL_BUILD.vcxproj /p:Configuration=Release || goto error
 ctest -V --output-on-failure -j4 || goto error
 
