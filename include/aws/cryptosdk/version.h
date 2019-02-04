ade6b533d6d15f01ebba5c4750d03c94ed35b024
Rajeev Joshi <43651355+jorajeev@users.noreply.github.com>
2019-02-04 12:22:21 -0800

    Updated to match README changes

--- include/aws/cryptosdk/version.h
+++ include/aws/cryptosdk/version.h
@@ -27,9 +27,8 @@
  * <a href="https://docs.aws.amazon.com/encryption-sdk/latest/developer-guide/introduction.html">Developer Guide</a>.
  *
  * <b>This library is currently under public preview.</b>
- * Feel free to check out the code and give it a spin, but be aware that the APIs might change
- * as we get closer to release. We'd love to hear your feedback on the APIs before they're
- * fully nailed down.
+ * Feel free to check out the code and give it a spin, but be aware that the APIs are still in flux. 
+ * We'd love to hear your feedback on the APIs before they're fully nailed down.
  *
  * Source code and installation instructions are avaiable in
  * <a href="https://github.com/awslabs/aws-encryption-sdk-c">the GitHub repository</a>.
