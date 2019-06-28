0b1cc72cbc73bdfa9e991907befdce18716c1273
lucasmt <lucasmt@amazon.com>
2019-06-28 14:33:06 -0400

    Relaxed assumptions in harness for aws_cryptosdk_md_abort

--- .cbmc-batch/jobs/aws_cryptosdk_md_abort/aws_cryptosdk_md_abort_harness.c
+++ .cbmc-batch/jobs/aws_cryptosdk_md_abort/aws_cryptosdk_md_abort_harness.c
@@ -8,10 +8,10 @@ void harness() {
     struct aws_cryptosdk_md_context *md_context = can_fail_malloc(sizeof(struct aws_cryptosdk_md_context));
 
     /* assumptions */
-    __CPROVER_assume(aws_cryptosdk_md_context_is_valid(md_context));
-    md_context->alloc      = can_fail_allocator();
-    md_context->evp_md_ctx = EVP_MD_CTX_new();
-    __CPROVER_assume(md_context->evp_md_ctx != NULL);
+    if (aws_cryptosdk_md_context_is_valid(md_context)) {
+        md_context->alloc      = can_fail_allocator();
+        md_context->evp_md_ctx = EVP_MD_CTX_new();
+    }
 
     /* operation under verification */
     aws_cryptosdk_md_abort(md_context);
