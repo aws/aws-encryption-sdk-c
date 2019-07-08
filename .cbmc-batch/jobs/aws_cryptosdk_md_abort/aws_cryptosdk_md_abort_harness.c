787c91df9d8114fef4fecdb1317996de7be2a433
lucasmt <lucasmt@amazon.com>
2019-07-08 18:16:37 -0400

    Proof harness for aws_cryptosdk_md_abort

---
+++ .cbmc-batch/jobs/aws_cryptosdk_md_abort/aws_cryptosdk_md_abort_harness.c
@@ -0,0 +1,18 @@
+#include <aws/cryptosdk/private/cipher.h>
+#include <proof_helpers/proof_allocators.h>
+
+#include <cipher_openssl.h>
+
+void harness() {
+    /* arguments */
+    struct aws_cryptosdk_md_context *md_context = can_fail_malloc(sizeof(struct aws_cryptosdk_md_context));
+
+    /* assumptions */
+    if (md_context) {
+        md_context->alloc      = can_fail_allocator();
+        md_context->evp_md_ctx = EVP_MD_CTX_new();
+    }
+
+    /* operation under verification */
+    aws_cryptosdk_md_abort(md_context);
+}
