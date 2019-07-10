a4d2d18bf057f7c9c61709b78e1ffc811e26cb67
lucasmt <lucasmt@amazon.com>
2019-07-10 13:35:11 -0400

    Added definition of aws_cryptosdk_sig_ctx to cipher_openssl.h

--- .cbmc-batch/include/cipher_openssl.h
+++ .cbmc-batch/include/cipher_openssl.h
@@ -1,5 +1,13 @@
-#include <aws/common/byte_buf.h>
 #include <aws/cryptosdk/private/cipher.h>
+
+struct aws_cryptosdk_sig_ctx {
+  struct aws_allocator *alloc;
+  const struct aws_cryptosdk_alg_properties *props;
+  EC_KEY *keypair;
+  EVP_PKEY *pkey;
+  EVP_MD_CTX *ctx;
+  bool is_sign;
+};
 
 struct aws_cryptosdk_md_context {
     struct aws_allocator *alloc;
