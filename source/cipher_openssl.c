552eb76833e0cb0931f290e825c72b059d919cf9
lucasmt <lucasmt@amazon.com>
2019-07-24 11:08:22 -0400

    Added pre and postconditions to aws_cryptosdk_md_* functions in cipher_openssl.c

--- source/cipher_openssl.c
+++ source/cipher_openssl.c
@@ -124,6 +124,7 @@ int aws_cryptosdk_md_init(
     (*md_context)->alloc      = alloc;
     (*md_context)->evp_md_ctx = evp_md_ctx;
 
+    AWS_POSTCONDITION(aws_cryptosdk_md_context_is_valid(*md_context));
     return AWS_OP_SUCCESS;
 err:
     EVP_MD_CTX_destroy(evp_md_ctx);
@@ -138,17 +139,27 @@ size_t aws_cryptosdk_md_size(enum aws_cryptosdk_md_alg md_alg) {
 }
 
 int aws_cryptosdk_md_update(struct aws_cryptosdk_md_context *md_context, const void *buf, size_t length) {
+    AWS_PRECONDITION(aws_cryptosdk_md_context_is_valid(md_context));
+    AWS_PRECONDITION(buf);
+    AWS_PRECONDITION(AWS_MEM_IS_READABLE(buf, length));
+
     if (1 != EVP_DigestUpdate(md_context->evp_md_ctx, buf, length)) {
+        AWS_POSTCONDITION(aws_cryptosdk_md_context_is_valid(md_context));
         return aws_raise_error(AWS_CRYPTOSDK_ERR_CRYPTO_UNKNOWN);
     }
 
+    AWS_POSTCONDITION(aws_cryptosdk_md_context_is_valid(md_context));
     return AWS_OP_SUCCESS;
 }
 
 int aws_cryptosdk_md_finish(struct aws_cryptosdk_md_context *md_context, void *output_buf, size_t *length) {
+    AWS_PRECONDITION(aws_cryptosdk_md_context_is_valid(md_context));
+    AWS_PRECONDITION(output_buf);
+
     int rv            = AWS_OP_SUCCESS;
     unsigned int size = 0;
 
+    // Replace with AWS_FATAL_PRECONDITION once that version is integrated
     if (!output_buf) {
         abort();
     }
