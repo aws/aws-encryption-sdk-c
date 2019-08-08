0b93a796cafe605750ecb89b7916aa30d1f33a04
Tegan Brennan <tegan@umail.ucsb.edu>
2019-08-08 13:52:05 -0400

    Updated proogs for add_record_c_str with new is_valid functions

--- .cbmc-batch/jobs/aws_cryptosdk_keyring_trace_add_record_c_str/aws_cryptosdk_keyring_trace_add_record_c_str_harness.c
+++ .cbmc-batch/jobs/aws_cryptosdk_keyring_trace_add_record_c_str/aws_cryptosdk_keyring_trace_add_record_c_str_harness.c
@@ -16,9 +16,10 @@
 #include <aws/common/array_list.h>
 #include <aws/common/string.h>
 #include <aws/cryptosdk/private/keyring_trace.h>
+#include <aws/cryptosdk/private/utils.h>
 #include <proof_helpers/make_common_data_structures.h>
 #include <proof_helpers/utils.h>
-#include <aws/cryptosdk/private/utils.h>
+#include <make_common_data_structures.h>
 
 
 void aws_cryptosdk_keyring_trace_add_record_c_str_harness() {
@@ -26,27 +27,27 @@ void aws_cryptosdk_keyring_trace_add_record_c_str_harness() {
     struct aws_allocator *alloc = can_fail_allocator(); /* Precondition: alloc must be non-null */
     struct aws_array_list trace;
     const char *c_str_namespace = ensure_c_str_is_allocated(MAX_BUFFER_SIZE);
-    const char *c_str_name = ensure_c_str_is_allocated(MAX_BUFFER_SIZE);
+    const char *c_str_name      = ensure_c_str_is_allocated(MAX_BUFFER_SIZE);
     uint32_t flags;
 
     __CPROVER_assume(aws_array_list_is_bounded(&trace, MAX_INITIAL_ITEM_ALLOCATION, MAX_ITEM_SIZE));
+    __CPROVER_assume(trace.item_size == sizeof(struct aws_cryptosdk_keyring_trace_record));
     ensure_array_list_has_allocated_data_member(&trace);
     __CPROVER_assume(aws_array_list_is_valid(&trace));
-
+    ensure_trace_has_allocated_records(&trace, MAX_STRING_LEN);
+    __CPROVER_assume(aws_cryptosdk_keyring_trace_is_valid(&trace));
 
     struct aws_array_list old = trace;
     struct store_byte_from_buffer old_byte;
     save_byte_from_array((uint8_t *)trace.data, trace.current_size, &old_byte);
 
-
-	if (aws_cryptosdk_keyring_trace_add_record_c_str(alloc, &trace, c_str_namespace, c_str_name, flags) == AWS_OP_SUCCESS){
+    if (aws_cryptosdk_keyring_trace_add_record_c_str(alloc, &trace, c_str_namespace, c_str_name, flags) ==
+        AWS_OP_SUCCESS) {
         /* assertions */
-        assert(aws_array_list_is_valid(&trace));
+        assert(aws_cryptosdk_keyring_trace_is_valid(&trace));
         assert(trace.length = old.length + 1);
-    }
-    else {
+    } else {
         /* assertions */
         assert_array_list_equivalence(&trace, &old, &old_byte);
     }
-
 }
