8580730000a2a83681b67e94890d580d98a2c402
Doug Chapman <dougch@amazon.com>
2019-08-14 13:49:59 -0700

    Dockerfile refactor to improve build times/success

--- docker-images/build.ps1
+++ docker-images/build.ps1
@@ -1,5 +1,6 @@
 Start-Transcript -path .\build.log
 
-docker build -m6g -t vs2015 .\visualcpp2015
-docker build -m6g -t vs2017 .\visualcpp2017
+docker build -m6g -t aws_encryption/windows_base:1809-amd64 -f .\windows_base\Dockerfile .
+docker build -m6g -t vs2015 -f .\visualcpp2015\Dockerfile .
+docker build -m6g -t vs2017 -f .\visualcpp2017\Dockerfile .
 
