39a3acfa1e723016b3ab37557ea9253664fdeb0b
Felipe R. Monteiro <felisous@amazon.com>
2020-03-06 16:34:07 -0500

    Adds a proof harness for aws_cryptosdk_rsa_encrypt function
    Signed-off-by: Felipe R. Monteiro <felisous@amazon.com>

--- .cbmc-batch/include/openssl/evp.h
+++ .cbmc-batch/include/openssl/evp.h
@@ -37,6 +37,11 @@ int EVP_PKEY_sign(EVP_PKEY_CTX *ctx, unsigned char *sig, size_t *siglen, const u
 void EVP_PKEY_CTX_free(EVP_PKEY_CTX *ctx);
 int EVP_PKEY_CTX_ctrl(EVP_PKEY_CTX *ctx, int keytype, int optype, int cmd, int p1, void *p2);
 int EVP_PKEY_derive(EVP_PKEY_CTX *ctx, unsigned char *key, size_t *keylen);
+int EVP_PKEY_encrypt_init(EVP_PKEY_CTX *ctx);
+int EVP_PKEY_CTX_set_rsa_padding(EVP_PKEY_CTX *ctx, int pad);
+int EVP_PKEY_CTX_set_rsa_oaep_md(EVP_PKEY_CTX *ctx, const EVP_MD *md);
+int EVP_PKEY_CTX_set_rsa_mgf1_md(EVP_PKEY_CTX *ctx, const EVP_MD *md);
+int EVP_PKEY_encrypt(EVP_PKEY_CTX *ctx, unsigned char *out, size_t *outlen, const unsigned char *in, size_t inlen);
 
 EVP_MD_CTX *EVP_MD_CTX_new(void);
 int EVP_MD_CTX_size(const EVP_MD_CTX *ctx);
--- .cbmc-batch/source/openssl/ec_override.c
+++ .cbmc-batch/source/openssl/ec_override.c
@@ -456,6 +456,19 @@ size_t max_derivation_size() {
     return derivation_size;
 }
 
+static size_t encryption_size;
+
+void initialize_max_encryption_size() {
+    size_t size;
+    // At different times, this value is stored in a size_t, a long and an int
+    __CPROVER_assume(0 < size && size <= INT_MAX);
+    encryption_size = size;
+}
+
+size_t max_encryption_size() {
+    return encryption_size;
+}
+
 /* Writes arbitrary data into the buffer out. */
 void write_unconstrained_data(unsigned char *out, size_t len) {
     assert(AWS_MEM_IS_WRITABLE(out, len));
