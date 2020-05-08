c0578f4fb742e778a95a0effc86adedd9996381b
ttjsu-aws <ttjsu@amazon.com>
2020-05-08 17:05:39 +0000

    Rebuild windows docker containers to support updates in CodeBuild Infra

--- docker-images/build.ps1
+++ docker-images/build.ps1
@@ -1,6 +1,6 @@
 Start-Transcript -path .\build.log
 
-docker build -m6g -t aws_encryption/windows_base:1607-KB4512517 .\windows_base
+docker build -m6g -t aws_encryption/windows_base:ltsc2016 .\windows_base
 docker build -m6g -t vs2015 .\visualcpp2015
 docker build -m6g -t vs2017 .\visualcpp2017
 
