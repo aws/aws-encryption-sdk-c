756f0a15a949dd954c08e3929faaeed1ea82e03f
tegansb <68670922+tegansb@users.noreply.github.com>
2020-11-26 18:24:11 -0800

    Adds CBMC proof for aws_cryptosdk_default_cmm_set_alg_id function (#636)
    * Update to ensure_cryptosdk_keyring_has_allocated_members
    
    * Adds harness, Makefile and yaml for aws_cryptosdk_default_cmm_set_alg_id
    
    * Adds preconditions to function
    
    * group declaration together and fix spacing in comments

--- source/default_cmm.c
+++ source/default_cmm.c
@@ -164,6 +164,7 @@ struct aws_cryptosdk_cmm *aws_cryptosdk_default_cmm_new(struct aws_allocator *al
 }
 
 int aws_cryptosdk_default_cmm_set_alg_id(struct aws_cryptosdk_cmm *cmm, enum aws_cryptosdk_alg_id alg_id) {
+    AWS_PRECONDITION(cmm != NULL);
     struct default_cmm *self = (struct default_cmm *)cmm;
     assert(self->base.vtable == &default_cmm_vt);
 
