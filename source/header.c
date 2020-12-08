38e29fbc7c30b05afdaeb93b75fb68c1c799dc48
Tegan Brennan <tegab@amazon.com>
2020-12-08 11:57:18 -0800

    Added alloc is valid check

--- source/header.c
+++ source/header.c
@@ -540,7 +540,7 @@ WRITE_ERR:
 
 bool aws_cryptosdk_hdr_is_valid(const struct aws_cryptosdk_hdr *hdr) {
     /* Header is not NULL and each field is a valid structure. */
-    return hdr != NULL && aws_cryptosdk_edk_list_is_valid(&hdr->edk_list) &&
+    return hdr != NULL && aws_allocator_is_valid(&hdr->alloc) && aws_cryptosdk_edk_list_is_valid(&hdr->edk_list) &&
            aws_cryptosdk_edk_list_elements_are_valid(&hdr->edk_list) && aws_byte_buf_is_valid(&hdr->iv) &&
            aws_byte_buf_is_valid(&hdr->auth_tag) && aws_byte_buf_is_valid(&hdr->message_id) &&
            aws_byte_buf_is_valid(&hdr->alg_suite_data) && aws_hash_table_is_valid(&hdr->enc_ctx);
