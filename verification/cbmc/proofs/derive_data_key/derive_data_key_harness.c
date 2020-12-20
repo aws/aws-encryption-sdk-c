8c1bf36ef94a69892a6bde01b16b6b41bc9d9edd
Adrian Palacios <accorell@amazon.com>
2020-12-20 13:50:31 +0100

    Simplifies proof and adds required variables to Makefile

--- verification/cbmc/proofs/derive_data_key/derive_data_key_harness.c
+++ verification/cbmc/proofs/derive_data_key/derive_data_key_harness.c
@@ -24,30 +24,13 @@
 #include <make_common_data_structures.h>
 
 void derive_data_key_harness() {
-    /* Nondet input */
-    struct aws_cryptosdk_session *session         = ensure_session_attempt_allocation(MAX_STRING_LEN);
-    struct aws_cryptosdk_dec_materials *materials = ensure_dec_materials_attempt_allocation();
+    /* Setup functions include nondet. allocation and common assumptions */
+    struct aws_cryptosdk_session *session         = session_setup(MAX_TABLE_SIZE, MAX_TRACE_LIST_ITEMS, MAX_EDK_LIST_ITEMS, MAX_BUFFER_SIZE, MAX_STRING_LEN);
+    struct aws_cryptosdk_dec_materials *materials = dec_materials_setup(MAX_TRACE_LIST_ITEMS, MAX_BUFFER_SIZE, MAX_STRING_LEN);
 
     /* Assumptions */
-    __CPROVER_assume(session != NULL);
     __CPROVER_assume(aws_cryptosdk_commitment_policy_is_valid(session->commitment_policy));
-
-    __CPROVER_assume(session->alg_props != NULL);
-    __CPROVER_assume(aws_cryptosdk_alg_properties_is_valid(session->alg_props));
     __CPROVER_assume(session->alg_props->commitment_len <= sizeof(session->key_commitment_arr));
-
-    __CPROVER_assume(aws_byte_buf_is_bounded(&session->header.message_id, MAX_BUFFER_SIZE));
-    ensure_byte_buf_has_allocated_buffer_member(&session->header.message_id);
-    __CPROVER_assume(aws_byte_buf_is_valid(&session->header.message_id));
-
-    __CPROVER_assume(aws_byte_buf_is_bounded(&session->header.alg_suite_data, MAX_BUFFER_SIZE));
-    ensure_byte_buf_has_allocated_buffer_member(&session->header.alg_suite_data);
-    __CPROVER_assume(aws_byte_buf_is_valid(&session->header.alg_suite_data));
-
-    __CPROVER_assume(materials != NULL);
-    __CPROVER_assume(aws_byte_buf_is_bounded(&materials->unencrypted_data_key, MAX_BUFFER_SIZE));
-    ensure_byte_buf_has_allocated_buffer_member(&materials->unencrypted_data_key);
-    __CPROVER_assume(aws_byte_buf_is_valid(&materials->unencrypted_data_key));
 
     /* Save current state of the data structures */
     struct aws_byte_buf *old_message_id = &session->header.message_id;
@@ -84,12 +67,10 @@ void derive_data_key_harness() {
             }
         }
     }
-    assert(aws_cryptosdk_alg_properties_is_valid(session->alg_props));
-    assert(aws_byte_buf_is_valid(&session->header.alg_suite_data));
+    assert(aws_cryptosdk_session_is_valid(session));
+    assert(aws_cryptosdk_dec_materials_is_valid(materials));
     assert_byte_buf_equivalence(&session->header.alg_suite_data, old_alg_suite_data, &old_byte_from_alg_suite_data);
-    assert(aws_byte_buf_is_valid(&session->header.message_id));
     assert_byte_buf_equivalence(&session->header.message_id, old_message_id, &old_byte_from_message_id);
-    assert(aws_byte_buf_is_valid(&materials->unencrypted_data_key));
     assert_byte_buf_equivalence(
         &materials->unencrypted_data_key, old_unencrypted_data_key, &old_byte_from_unencrypted_data_key);
 }
