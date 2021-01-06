ebfdd1a3daf52a9d0f4620b687b27c599a338a27
Adrian Palacios <73246657+adpaco-aws@users.noreply.github.com>
2021-01-06 16:02:20 +0100

    Add CBMC proof for default_cmm_generate_enc_materials (#656)
    * Add proof files for default_cmm_generate_enc_materials
    
    * Move on_encrypt function to its own file
    
    * Add validator and allocator functions
    Change variable name and add comment for it on Makefile
    Merge changes from 'master'
    
    * Add and edit allocators for materials and cmm
    
    * Add missing file to Makefile (algorithm_is_known)
    
    * Remove unnecessary assumptions
    
    * Edit preconditions and postconditions
    
    * Fix style checks
    
    * Restore simpler precondition
    
    * Check that output remains unchanged (top-level check only)
    Do intial allocation as usual and pass output by reference
    Remove request_alg assertion in case of failure
    
    * Fix style issues
    
    * Use C-style comments and add space
    
    * Remove unneeded assumptions and use nondet values
    Replace precondition by false case in enc_request validator
    
    * Remove copyright year on Makefile

--- verification/cbmc/proofs/aws_cryptosdk_keyring_on_encrypt/aws_cryptosdk_keyring_on_encrypt_harness.c
+++ verification/cbmc/proofs/aws_cryptosdk_keyring_on_encrypt/aws_cryptosdk_keyring_on_encrypt_harness.c
@@ -26,18 +26,6 @@
 #include <proof_helpers/proof_allocators.h>
 #include <proof_helpers/utils.h>
 
-/**
- * Stub for the virtual function on_encrypt of a aws_cryptosdk_keyring_vt structure.
- * Must implement if used for encryption.
- * It should be as nondeterministic as possible to increase coverage on the operation under
- * verfication; thus, it also triggers states with errors.
- *
- * When the buffer for the unencrypted data key is not NULL at the time of the call, it
- * must not be changed by callee. All buffers for EDKs pushed onto the list must be in a
- * valid state, which means either that they are set to all zeroes or that they have been
- * initialized using one of the byte buffer initialization functions. This assures proper
- * clean up and serialization.
- */
 int on_encrypt(
     struct aws_cryptosdk_keyring *keyring,
     struct aws_allocator *request_alloc,
@@ -45,40 +33,7 @@ int on_encrypt(
     struct aws_array_list *keyring_trace,
     struct aws_array_list *edks,
     const struct aws_hash_table *enc_ctx,
-    enum aws_cryptosdk_alg_id alg) {
-    /* Check validity for all inputs to avoid memory-safety violations. */
-    assert(aws_cryptosdk_keyring_is_valid(keyring));
-    assert(aws_allocator_is_valid(request_alloc));
-    assert(aws_byte_buf_is_valid(unencrypted_data_key));
-    assert(aws_cryptosdk_keyring_trace_is_valid(keyring_trace));
-    assert(aws_cryptosdk_edk_list_is_valid(edks));
-    assert(aws_cryptosdk_edk_list_elements_are_valid(edks));
-    assert((enc_ctx == NULL) || aws_hash_table_is_valid(enc_ctx));
-
-    if (unencrypted_data_key->buffer == NULL) {
-        const struct aws_cryptosdk_alg_properties *props = aws_cryptosdk_alg_props(alg);
-        if (props != NULL) {
-            __CPROVER_assume(aws_byte_buf_is_bounded(unencrypted_data_key, props->data_key_len));
-            ensure_byte_buf_has_allocated_buffer_member(unencrypted_data_key);
-            /*
-             * This satisfies the post-condition that if this keyring
-             * generated data key, it must be the right length.
-             * The nondeterminism increases coverage.
-             */
-            if (nondet_bool()) unencrypted_data_key->len = props->data_key_len;
-            unencrypted_data_key->allocator = request_alloc;
-            __CPROVER_assume(aws_byte_buf_is_valid(unencrypted_data_key));
-        }
-    } else {
-        /*
-         * If the buffer is not NULL, the byte buffer must not have been modified;
-         * however, this allocation modifies the buffer and increases coverage.
-         */
-        if (nondet_bool()) ensure_byte_buf_has_allocated_buffer_member(unencrypted_data_key);
-    }
-    int ret;
-    return ret;
-}
+    enum aws_cryptosdk_alg_id alg);
 
 void aws_cryptosdk_keyring_on_encrypt_harness() {
     /* Non-deterministic inputs. */
