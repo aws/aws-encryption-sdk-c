6943c24545bb3f84f626c5fd1b88f8e85fa91031
Bryce Shang <shanggu@amazon.com>
2021-06-22 15:24:31 -0700

    Fix clang-format issue.

--- source/cipher_openssl.c
+++ source/cipher_openssl.c
@@ -19,11 +19,11 @@
 // TODO(CryptoAlg-778): address build with awslc on windows.
 #if defined(__linux__)
 #    include <openssl/base.h>
-#if defined(OPENSSL_IS_AWSLC)
-#    include <openssl/asn1.h>
-#    include <openssl/asn1t.h>
-#    include <openssl/obj.h>
-#endif
+#    if defined(OPENSSL_IS_AWSLC)
+#        include <openssl/asn1.h>
+#        include <openssl/asn1t.h>
+#        include <openssl/obj.h>
+#    endif
 #endif
 
 #include <openssl/crypto.h>
