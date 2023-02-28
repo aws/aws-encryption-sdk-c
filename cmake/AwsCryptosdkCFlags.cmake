5ed884abe4d758a9833886f36955f42e7bde526e
Andy Jewell <ajewell@amazon.com>
2023-02-28 10:02:14 -0500

    remove Werror

--- cmake/AwsCryptosdkCFlags.cmake
+++ cmake/AwsCryptosdkCFlags.cmake
@@ -60,9 +60,7 @@ function(aws_cryptosdk_set_common_properties target)
         list(APPEND AWS_C_FLAGS /wd4706) # assignment within conditional
         list(APPEND AWS_C_FLAGS /wd5045) # "Compiler will insert spectre mitigations if /Qspectre specified"
     else()
-        # list(APPEND AWS_C_FLAGS -Wall -Werror -Wreturn-type)
-	# the aws-cpp-sdk has some warnings, and so the build fails of we use -Werror
-        list(APPEND AWS_C_FLAGS -Wall -Wreturn-type -Wno-shorten-64-to-32)
+        list(APPEND AWS_C_FLAGS -Wall -Werror -Wreturn-type)
 
         if(NOT SET_PROPERTIES_NO_WEXTRA)
             list(APPEND AWS_C_FLAGS -Wextra)
